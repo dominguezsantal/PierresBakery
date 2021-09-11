7528d364da0e8f6b7d97cfe0e63ab97fe2d4ccb7
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-11 14:48:33 -0700

    add Bread.cs,Pastry.cs, Bread.Tests.cs and Pastry.Tests.cs

---
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MainBakery.Models;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+
+    public class PastryTests
+    {
+
+    }
+} 
---
+++ MainBakery/Models/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery
+{
+    public class Program
+    {
+        public static void Main();
+
+
+
+    }
+} 
