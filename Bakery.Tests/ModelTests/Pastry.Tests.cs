b521bc9b80a54d49862fd46ae53881b839117417
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-11 17:26:16 -0700

    Fabricated Test 2: misspelled pastry's object (newPastry to regPastry)

--- Bakery.Tests/ModelTests/Pastry.Tests.cs
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -7,6 +7,11 @@ namespace Bakery.Tests
 
     public class PastryTests
     {
-
+    [TestMethod]
+        public void GetBread_CreatesInstanceOfPastry_Pastry()
+        {
+            Pastry newPastry = new Pastry(0, 0);
+            Assert.AreEqual(typeof(Pastry), regPastry.GetType());
+        }
     }
 } 
